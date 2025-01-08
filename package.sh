79939df032e45fbddaa8b2235095e61f381fe01b
James Cuzella <james.cuzella@lyraphase.com>
2025-01-07 19:47:11 -0700

    package.sh: Fix typo: --vagrantfile

--- package.sh
+++ package.sh
@@ -21,4 +21,4 @@ box_filename=${1:-lyraphase-runner-macos-monterey-12-1-base.box}
 
 # Tell Vagrantfile to avoid replacing SSH key & syncing Shared Folders
 export VAGRANT_PACKAGE='true'
-vagrant package --vagranfile Vagrantfile  --info info.json  --include README.md,LICENSE-macOSMonterey.pdf  --output "$box_filename"
+vagrant package --vagrantfile Vagrantfile  --info info.json  --include README.md,LICENSE-macOSMonterey.pdf  --output "$box_filename"
